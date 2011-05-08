35e98ba9d25fd41549f9dd4015ed7dd4534f2cb8
unclaimed <unclaimed00@gmail.com>
2011-05-08 15:32:31 -0300

    201105081532

--- src-trace/cl/niclabs/skandium/trace/SkeletonTreeBuilder.java
+++ src-trace/cl/niclabs/skandium/trace/SkeletonTreeBuilder.java
@@ -17,14 +17,12 @@ import cl.niclabs.skandium.skeletons.While;
 
 class SkeletonTreeBuilder implements SkeletonVisitor {
 	
+	private static final double SKEL_SIDE = 50;
+	private static final double MUS_SIDE = SKEL_SIDE/2;
 	private static final String STYLE_SKEL = "defaultVertex;shape=triangle;direction=north;verticalAlign=bottom";
 	private static final String STYLE_MUS = "defaultVertex;shape=ellipse;direction=north;verticalAlign=bottom;fillColor=white";
 	private static final String STYLE_EDGE = "endArrow=none";
 	private static final String STYLE_NOSHAPE = "shape=none;foldable=0";
-	private static final double SKEL_WIDTH = 50;
-	private static final double SKEL_HEIGHT = 50;
-	private static final double MUS_WIDTH = 23;
-	private static final double MUS_HEIGHT = 23;
 	private static final String CONDITION = "C";
 	private static final String SPLIT = "S";
 	private static final String MERGE = "M";
@@ -37,6 +35,93 @@ class SkeletonTreeBuilder implements SkeletonVisitor {
 		super();
 		this.graph = graph;
 		this.parent = parent;
+	}
+
+	@Override
+	public <P, R> void visit(Seq<P, R> skeleton) {
+		mxCell skelVert = (mxCell) graph.insertVertex(parent,null,"Seq",0,0,SKEL_SIDE*0.92,SKEL_SIDE*0.92,STYLE_SKEL);
+		skelVert.setConnectable(false);
+		mxCell exeVert = (mxCell) graph.insertVertex(parent,null,EXECUTE,MUS_SIDE/2,SKEL_SIDE*1.4,MUS_SIDE*0.92,MUS_SIDE*0.92,STYLE_MUS);
+		exeVert.setConnectable(false);
+		graph.insertEdge(parent, null, "", skelVert, exeVert, STYLE_EDGE);
+	}
+
+	@Override
+	public <P, R> void visit(Farm<P, R> skeleton) {
+		mxCell subSkelVert = (mxCell) graph.insertVertex(parent,null,"",0,SKEL_SIDE*1.4,0,0,STYLE_NOSHAPE);
+		subSkelVert.setConnectable(false);
+
+		SkeletonTreeBuilder subSkelBulder = new SkeletonTreeBuilder(graph,subSkelVert);
+		skeleton.getSubskel().accept(subSkelBulder);		
+		
+		double w = subSkelVert.getGeometry().getWidth();
+		double x = w>SKEL_SIDE? (w/2) - (SKEL_SIDE/2): 0;
+		
+		mxCell skelVert = (mxCell) graph.insertVertex(parent,null,"Farm",x,0,SKEL_SIDE*0.92,SKEL_SIDE*0.92,STYLE_SKEL);
+		skelVert.setConnectable(false);
+		graph.insertEdge(parent, null, "", skelVert, subSkelVert, STYLE_EDGE);		
+	}
+
+	@Override
+	public <P, R> void visit(Pipe<P, R> skeleton) {
+		mxCell stage1Vert = (mxCell) graph.insertVertex(parent,null,"",0,SKEL_SIDE*1.4,0,0,STYLE_NOSHAPE);
+		stage1Vert.setConnectable(false);
+
+		SkeletonTreeBuilder stage1Bulder = new SkeletonTreeBuilder(graph,stage1Vert);
+		skeleton.getStage1().accept(stage1Bulder);		
+		
+		double w1 = stage1Vert.getGeometry().getWidth();
+		
+		mxCell stage2Vert = (mxCell) graph.insertVertex(parent,null,"",w1,SKEL_SIDE*1.4,0,0,STYLE_NOSHAPE);
+		stage2Vert.setConnectable(false);
+
+		SkeletonTreeBuilder stage2Bulder = new SkeletonTreeBuilder(graph,stage2Vert);
+		skeleton.getStage2().accept(stage2Bulder);		
+		
+		double w2 = stage2Vert.getGeometry().getWidth();
+		
+		double x = ((w1 + w2)/2) - (SKEL_SIDE/2);
+		
+		mxCell skelVert = (mxCell) graph.insertVertex(parent,null,"Pipe",x,0,SKEL_SIDE*0.92,SKEL_SIDE*0.92,STYLE_SKEL);
+		skelVert.setConnectable(false);
+		graph.insertEdge(parent, null, "", skelVert, stage1Vert, STYLE_EDGE);		
+		graph.insertEdge(parent, null, "", skelVert, stage2Vert, STYLE_EDGE);		
+	}
+
+	@Override
+	public <P> void visit(For<P> skeleton) {
+		mxCell subSkelVert = (mxCell) graph.insertVertex(parent,null,"",0,SKEL_SIDE*1.4,0,0,STYLE_NOSHAPE);
+		subSkelVert.setConnectable(false);
+
+		SkeletonTreeBuilder subSkelBulder = new SkeletonTreeBuilder(graph,subSkelVert);
+		skeleton.getSubskel().accept(subSkelBulder);		
+		
+		double w = subSkelVert.getGeometry().getWidth();
+		double x = w>SKEL_SIDE? (w/2) - (SKEL_SIDE/2): 0;
+		
+		mxCell skelVert = (mxCell) graph.insertVertex(parent,null,"For",x,0,SKEL_SIDE*0.92,SKEL_SIDE*0.92,STYLE_SKEL);
+		skelVert.setConnectable(false);
+		graph.insertEdge(parent, null, "", skelVert, subSkelVert, STYLE_EDGE);		
+	}
+
+	
+	
+	
+	
+	@Override
+	public <P, R> void visit(If<P, R> skeleton) {
+		mxCell conVert = (mxCell) graph.insertVertex(parent,null,CONDITION,0,SKEL_SIDE*1.4,MUS_SIDE,MUS_SIDE,STYLE_MUS);
+		conVert.setConnectable(false);
+
+		mxCell subSkelVert = (mxCell) graph.insertVertex(parent,null,"",25,70,0,0,STYLE_NOSHAPE);
+		subSkelVert.setConnectable(false);
+
+		SkeletonTreeBuilder subSkelBulder = new SkeletonTreeBuilder(graph,subSkelVert);
+		skeleton.getSubskel().accept(subSkelBulder);		
+		
+		double w = subSkelVert.getGeometry().getWidth();
+		double x = w>50? (w/2) - 25: 0;
+		
 	}
 
 	@Override
@@ -61,43 +146,7 @@ class SkeletonTreeBuilder implements SkeletonVisitor {
 	}
 
 	@Override
-	public <P, R> void visit(Farm<P, R> skeleton) {
-		mxCell farmVert = (mxCell) graph.insertVertex(parent,null,"Farm",0,0,SKEL_WIDTH,SKEL_HEIGHT,STYLE_SKEL);
-		farmVert.setConnectable(false);
-		mxCell subSkelVert = (mxCell) graph.insertVertex(parent,null,"",0,70,0,0,STYLE_NOSHAPE);
-		subSkelVert.setConnectable(false);
-		graph.insertEdge(parent, null, "", farmVert, subSkelVert, STYLE_EDGE);
-		
-		SkeletonTreeBuilder subSkelBulder = new SkeletonTreeBuilder(graph,subSkelVert);
-		skeleton.getSubskel().accept(subSkelBulder);		
-	}
-
-	@Override
-	public <P, R> void visit(Pipe<P, R> skeleton) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <P, R> void visit(Seq<P, R> skeleton) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <P, R> void visit(If<P, R> skeleton) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public <P> void visit(While<P> skeleton) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <P> void visit(For<P> skeleton) {
 		// TODO Auto-generated method stub
 		
 	}
